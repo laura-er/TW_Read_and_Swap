a4057f72387fac429375e07c2aeaae8f1c44b4a8
adelina-ist <istratiadelina4@gmail.com>
2026-03-29 06:18:10 +0300

    add SwapRequest entity, context and migration

--- backend/BookSwap.DataAccessLayer/Context/BookSwapDbContext.cs
+++ backend/BookSwap.DataAccessLayer/Context/BookSwapDbContext.cs
@@ -1,11 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using BookSwap.Domain.Entities.User;
 using BookSwap.Domain.Entities.Book;
+using BookSwap.Domain.Entities.Swap;
 namespace BookSwap.DataAccessLayer.Context;
 
 public sealed class BookSwapDbContext : DbContext
 {
     public DbSet<UserEntity> Users { get; set; }
     public DbSet<BookEntity> Books { get; set; }
+    public DbSet<SwapRequestEntity> SwapRequests { get; set; }
     public BookSwapDbContext(DbContextOptions<BookSwapDbContext> options) : base(options) { }
 }
